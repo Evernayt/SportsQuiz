f35a30e4f7018e1d406ff535dbbc4c78c1e5d6cc
Evernayt <evernayte@gmail.com>
2022-10-16 13:57:38 +0500

    Added reset state

--- Context.ts
+++ Context.ts
@@ -4,11 +4,15 @@ import {ISelectedAnswer} from './src/models/ISelectedAnswer';
 interface IContext {
   selectedAnswers: ISelectedAnswer[];
   setSelectedAnswers: (v: ISelectedAnswer[]) => void;
+  reset: boolean;
+  setReset: (v: any) => void;
 }
 
 const defaultState: IContext = {
   selectedAnswers: [],
   setSelectedAnswers: () => {},
+  reset: false,
+  setReset: () => {},
 };
 
 const Context = createContext<IContext>(defaultState);
